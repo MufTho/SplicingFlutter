c540ae8a790d87e529912eb2a37c4872013740b0
Mufid musthofa <mufid.musthofa7@gmail.com>
2022-05-24 13:44:57 +0700

    update theme

--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -32,3 +32,42 @@ TextStyle decsTextStyle = GoogleFonts.poppins(
   fontSize: 16,
   fontWeight: FontWeight.w600,
 );
+
+TextStyle foodTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle pricingTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle questionTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle rateTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle firstTextStyle = GoogleFonts.poppins(
+  color: const Color(0xff121622),
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle subTextStyle = GoogleFonts.poppins(
+  color: const Color(0xff808EAB),
+  fontSize: 16,
+);
+
+TextStyle massageTextStyle = GoogleFonts.poppins(
+  color: const Color(0xff808EAB),
+);
