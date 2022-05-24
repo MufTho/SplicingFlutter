bc35f99e86c0485bcc2e48dc8b09adc2d7321a38
Mufid musthofa <mufid.musthofa7@gmail.com>
2022-05-24 12:58:17 +0700

    Add Theme TextStyle

---
+++ lib/widgets/theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blackColor = Colors.black;
+Color whiteColor = Colors.white;
+
+TextStyle boldTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle lightTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontSize: 16,
+);
+
+TextStyle buttonStyle = GoogleFonts.poppins(
+  color: Colors.white,
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle decsTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
